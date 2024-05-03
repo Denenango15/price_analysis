b6f8a7b5c90a030072be90484fb4f2fe0ecc14c0
Denis <denisnesterov15@gmail.com>
2024-05-03 10:39:05 +0400

    added the ability to select an arbitrary period

--- data_download.py
+++ data_download.py
@@ -1,19 +1,26 @@
 import yfinance as yf
 
-def fetch_stock_data(ticker, period='1mo'):
+
+def fetch_stock_data(ticker, start_date=None, end_date=None, period='1mo'):
     """
-        Функция для загрузки данных о биржевых запасах.
+    Функция для загрузки данных о биржевых запасах.
 
-        Параметры:
-            ticker (str): Тикер акции.
-            period (str, опционально): Период времени для загрузки данных (по умолчанию '1mo' - один месяц).
+    Параметры:
+        ticker (str): Тикер акции.
+        start_date (str, опционально): Начальная дата для загрузки данных (по умолчанию None).
+        end_date (str, опционально): Конечная дата для загрузки данных (по умолчанию None).
+        period (str, опционально): Период времени для загрузки данных (по умолчанию '1mo' - один месяц).
 
-        Возвращает:
-            pd.DataFrame: DataFrame с данными о биржевых запасах.
-        """
-    stock = yf.Ticker(ticker)
-    data = stock.history(period=period)
+    Возвращает:
+        pd.DataFrame: DataFrame с данными о биржевых запасах.
+    """
+    if start_date is not None and end_date is not None:
+        data = yf.download(ticker, start=start_date, end=end_date, progress=False) # progress = True if your need notification about progress
+    else:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period=period)
     return data
+
 
 def add_moving_average(data, window_size=5):
     """
@@ -28,6 +35,7 @@ def add_moving_average(data, window_size=5):
         """
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
+
 
 def calculate_rsi(data, window=14):
     """
@@ -46,6 +54,7 @@ def calculate_rsi(data, window=14):
     rs = gain / loss
     rsi = 100 - (100 / (1 + rs))
     return rsi
+
 
 def calculate_macd(data, short_window=12, long_window=26, signal_window=9):
     """
@@ -66,6 +75,7 @@ def calculate_macd(data, short_window=12, long_window=26, signal_window=9):
     signal_line = macd_line.ewm(span=signal_window, min_periods=1, adjust=False).mean()
     return macd_line, signal_line
 
+
 def calculate_and_display_average_price(data):
     """
        Функция для вычисления и отображения средней цены закрытия акций.
@@ -75,6 +85,7 @@ def calculate_and_display_average_price(data):
        """
     average_price = data['Close'].mean()
     print(f"Средняя цена закрытия акций: {average_price}")
+
 
 def notify_if_strong_fluctuations(data, threshold):
     """
@@ -94,6 +105,7 @@ def notify_if_strong_fluctuations(data, threshold):
         print(f"Цена акций колебалась более чем на {threshold}% за период.")
     else:
         print("Цена акций не колебалась на заданный процент за период.")
+
 
 def export_data_to_csv(data, filename):
     """
--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import data_download as dd
 import data_plotting as dplt
 
 def main():
-    """
-           Основная функция для получения данных о биржевых акциях, построения графика и анализа колебаний цен.
-           The main function is to obtain data on stock exchanges, plotting and analyzing price fluctuations.
-        """
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
     print(
         "Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
     print(
         "Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
-    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
-    period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): ")
+    period = input("Введите период для данных (например, '1mo' для одного месяца), или или нажмите Enter, чтобы установить произвольную дату: ")
 
-    # Fetch stock data
-    stock_data = dd.fetch_stock_data(ticker, period)
+    start_date = input(
+        "Введите начальную дату в формате 'ГГГГ-ММ-ДД' (например, '2023-01-01'), или нажмите Enter для использования предустановленного периода: ")
+    end_date = input(
+        "Введите конечную дату в формате 'ГГГГ-ММ-ДД' (например, '2023-12-31'), или нажмите Enter для использования предустановленного периода: ")
+
+    if start_date and end_date:  # Если даты начала и окончания указаны
+        stock_data = dd.fetch_stock_data(ticker, start_date=start_date, end_date=end_date)
+    else:
+        stock_data = dd.fetch_stock_data(ticker, period=period)
+
+    if stock_data.empty:
+        print("Данные не загружены. Проверьте введенные данные и повторите попытку.")
+        return
 
     # Add moving average to the data
     stock_data = dd.add_moving_average(stock_data)
@@ -37,3 +44,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
